464bf5f8a08f1ff7f13c88e52316c8e0271e3dce
kajolmalhotra <75560211+kajolmalhotra@users.noreply.github.com>
2024-02-29 17:59:04 +0530

    Initial commit

---
+++ credentials.py
@@ -0,0 +1,6 @@
+
+
+consumer_key = 'D98191QMPXByulOy23dwwoTAY'
+consumer_secret = '59yAW69GNHTp5MRaxjicRi3d8iB1l6ereeMbE4bcHwZxxdmO7y'
+access_token = '1569646705553059845-xMbbuElJtABrHrkgUzg21FXglitNC2'
+access_token_secret = '8q7VEgxZC2tqoICBDqnEGK9w2eYR54kGu5n7sjXB2n8vQ'
